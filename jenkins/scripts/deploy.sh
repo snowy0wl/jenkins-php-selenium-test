bdefb365c16fd77e0fdef36ea5f5206a2d3d1a2e
snowy0wl <48132227+snowy0wl@users.noreply.github.com>
2020-11-08 02:51:23 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v "$PWD"/src php:7.2-apache              
+docker run -d -p 80:80 --name my-apache-php-app -v /root/jenkins-php-selenium-test/src php:7.2-apache              
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://fck la to see your PHP application in action.'
-
