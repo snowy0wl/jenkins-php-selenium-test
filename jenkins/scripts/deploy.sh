4c8d5c8c81f8c785d62b78936361992fa51d65b3
snowy0wl <48132227+snowy0wl@users.noreply.github.com>
2020-11-08 02:33:06 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v "$PWD":/root/jenkins-php-selenium-test/jenkins-php-selenium-test/src php:7.2-apache              
+docker run -d -p 80:80 --name my-apache-php-app -v "$PWD":/root/jenkins-php-selenium-test/src php:7.2-apache              
 sleep 1
 set +x
 
 echo 'Now...'
 echo 'Visit http://fck la to see your PHP application in action.'
+
+
+
+
+docker run --rm --detach -u root --name my-apache-php-app
+--volume "$PWD":/root/jenkins-php-selenium-test/jenkins-php-selenium-test/src
+--volume "%HOMEDRIVE%%HOMEPATH%":/home
+--volume /var/run/docker.sock:/var/run/docker.sock
+--publish 8080:8080 jenkinsci/blueocean
 
